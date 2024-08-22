78aa56cb58417b8e5e59e3f4ad6b02d4f28c6656
Oychekhra <rubyhankuk@gmail.com>
2024-08-22 18:58:43 +0900

    feat: reja.ejs updated

--- app.js
+++ app.js
@@ -38,16 +38,38 @@ app.set("view engine", "ejs");
 //     res.end("<h1 style='color: blue'>You are at gifts page</h1>");
 // });
 app.post("/create-item", (req, res) => {
+    console.log("user entered /create-item");
     console.log(req.body);
-    res.json({test: "success"});
-})
+    // res.json({test: "success"});
+    // res.end("success");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else{
+            res.end("successfully added");
+        }  
+    })
+});
 
 app.get("/author", (req, res) => {
    res.render("author", {user: user});
-})
+});
 
 app.get('/', function(red, res) {
-    res.render('reja');
+    console.log("user entered /");
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else{
+            // console.log(data);
+            res.render("reja", {items: data});
+        }
+    })
 });
 
 
