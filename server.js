9a974a7ae5ea2785719060f32e72710ed2b8e571
Oychekhra <rubyhankuk@gmail.com>
2024-08-22 18:06:45 +0900

    feat: mongoDB ulanish

--- server.js
+++ server.js
@@ -1,57 +1,24 @@
-console.log("web serverni boshlash");
-const { Console } = require("console");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb =require("mongodb");
 
+let db;
+const connectionString ="mongodb+srv://Oychehra:yRSa3XHT6tMh-Gx@cluster0.miu5g.mongodb.net/Reja"
 
-let user;
-fs.readFile("database/user.json", "utf8",(err, data) => {
-    if(err) {
-        console.log("ERROR", err);
-    } else {
-        user = JSON.parse(data)
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err, client) =>{
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+    });
     }
-});
+}
+);
 
-
-
-// 1 => Kirish code
-app.use(express.static("public")); //=> har qanday browserdan kirib kelayotgan sorovnomalar uchun faqat  public folder ochiq  
-app.use(express.json()); // => kirib kelayotgan json formatdagi data object holatga ozgartirib beradi
-app.use(express.urlencoded({extended: true})); // => htmldan traditional request (form) larni qabul qiladi
-
-// 2 => Session code
-
-// 3 Views code (backendda frontend yasash)
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-// 4 Routing code
-// app.get("/hello", function(req, res) {
-//         res.end("<h1 style='background: red'>HELLO WORLD by Oychehra</h1>");
-// });
-
-// app.get("/gift", function(req, res) {
-//     res.end("<h1 style='color: blue'>You are at gifts page</h1>");
-// });
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-})
-
-app.get("/author", (req, res) => {
-   res.render("author", {user: user});
-})
-
-app.get('/', function(red, res) {
-    res.render('reja');
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
