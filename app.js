fd3152af5ffa0c587761ccecaacbc4273448ab52
Oychekhra <rubyhankuk@gmail.com>
2024-08-24 12:56:48 +0900

    feat: Reja delete button upgraded

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ fs.readFile("database/user.json", "utf8",(err, data) => {
 
 //MongoDB connect
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1 => Kirish code
 app.use(express.static("public")); //=> har qanday browserdan kirib kelayotgan sorovnomalar uchun faqat  public folder ochiq  
@@ -47,6 +48,15 @@ app.post("/create-item", (req, res) => {
        console.log(data.ops);
         res.json(data.ops[0]);
     });
+});
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, 
+    function(err, data){
+        res.json({state: "success"});
+    })
+
 });
 
 app.get("/author", (req, res) => {
