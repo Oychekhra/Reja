6524a62c4362f12fe450e677e0f8f621b3e75928
Oychekhra <rubyhankuk@gmail.com>
2024-08-24 09:08:19 +0900

    feat: MITASK- C uploaded

--- train.js
+++ train.js
@@ -130,18 +130,70 @@ MASALAN countLetter("e", "engineer") 3ni return qiladi.*/
 
 
 
+/*B-TASK*/
 
-const countNumber = (anyword) => {
-    let count = 0;
-    const number = []
-    for(let i = 0; i < anyword.length; i++){
-        if( Number(anyword[i]) || parseInt(anyword[i]) === 0) {
-            count++;
-            number.push(anyword[i])
+//Answer
+// const countNumber = (anyword) => {
+//     let count = 0;
+//     const number = []
+//     for(let i = 0; i < anyword.length; i++){
+//         if( Number(anyword[i]) || parseInt(anyword[i]) === 0) {
+//             count++;
+//             number.push(anyword[i])
+//         }
+//     }
+//     console.log(`Count of numbers in ${anyword}  equals to =>  ${count}`);
+//     console.log('Numbers inside given string are:', number);
+// }
+
+// countNumber( "ad2a54y79wet0sfgb9");
+
+
+/*C-TASK
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin,
+hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. 
+Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); 
+shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
+ shop.sotish('non', 3) & shop.qabul('cola', 4) & 
+ shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud*/
+
+//Answer
+ const moment = require ('moment');
+let now = moment().format('h:mm');
+ class Shop {
+    non;
+    lagmon;
+    cola;
+
+
+
+    constructor(non, lagmon, cola) {
+        this.non = non;
+        this.lagmon =lagmon;
+        this.cola = cola;
+    }
+    qoldiq() {
+        console.log(`hozir ${now}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud`)
+    }
+
+    sotish(product, miqdor) {
+        if(this[product] !== undefined && this[product]>= miqdor) {
+            this[product] -= miqdor;
+            console.log(`hozir ${now}da ${miqdor}ta ${product} sotildi va dokonda ${this[product]}ta qoldi`)
+        }else{
+            console.log(`hozir ${now} zahirada sotib olish uchun  ${product} yetarli emas.`)
         }
     }
-    console.log(`Count of numbers in ${anyword}  equals to =>  ${count}`);
-    console.log('Numbers inside given string are:', number);
+
+    qabul(product, miqdor) {
+        this[product]+= miqdor;
+        console.log(`Hozir ${now}da ${miqdor}ta ${product} sotib olindi va dokonda ${this[product]}ta boldi`)
+    }
 }
 
-countNumber( "ad2a54y79wet0sfgb9");
+const Shop1 = new Shop(4, 5, 2);
+Shop1.qoldiq();
+Shop1.sotish("non", 3);
+Shop1.qabul('cola', 4)
+Shop1.qoldiq();
